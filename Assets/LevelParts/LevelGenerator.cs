aabfa718851fc38f018e7cbec7f95017d1042e20
memo <alexis45645@gmail.com>
2023-11-06 03:26:20 -0500

    Movement, New Input System, Cinemachine

--- Assets/LevelParts/LevelGenerator.cs
+++ Assets/LevelParts/LevelGenerator.cs
@@ -1,22 +1,19 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class LevelGenerator : MonoBehaviour
 {
-    [SerializeField] 
-    [Tooltip("How many level parts will be generated at any given time.")]
+    [SerializeField, Tooltip("How many level parts will be generated at any given time.")]
     private int genCount = 3;
 
-    [SerializeField]
-    [Tooltip("Parts from this list will be randomly selected for generation.")]
+    [SerializeField, Tooltip("Parts from this list will be randomly selected for generation.")]
     private List<GameObject> possibleParts;
 
-    private List<GameObject> levelParts = new List<GameObject>();
+    private List<GameObject> levelParts = new();
 
     private void Start()
     {
-        for(int ii = 0; ii < genCount; ii++)
+        for(int i = 0; i < genCount; ++i)
         {
             AddNewPart();
         }
@@ -30,10 +27,10 @@ public class LevelGenerator : MonoBehaviour
     public void AddNewPart()
     {
         levelParts.Add(Instantiate(RandomPart()));
-        levelParts[levelParts.Count-1].transform.parent = transform;
+        levelParts[^1].transform.parent = transform;
 
-        levelParts[levelParts.Count - 1].transform.position = levelParts.Count == 1 ?
-            Vector3.zero : levelParts[levelParts.Count - 2].GetComponent<PartScript>().connectPoint.position;
+        levelParts[^1].transform.position = levelParts.Count == 1 ?
+            Vector3.zero : levelParts[^2].GetComponent<PartScript>().connectPoint.position;
 
         if (levelParts.Count > genCount) RemoveOldestPart();
     }
