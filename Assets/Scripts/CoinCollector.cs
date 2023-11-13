ed7fe03ecca909355a4bc1e65a98d1e53530f25d
greatbrick <speharley@gmail.com>
2023-11-13 18:23:26 -0500

    Added Coin Sounds

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -7,11 +7,24 @@ public class CoinCollector : MonoBehaviour
     [field: SerializeField]
     public int coins { get; private set; } = 0;
 
+    [Header("Coin Boost")]
     [SerializeField]
     private int coinBoostCount = 3;
     private int coinCounter = 0;
     [SerializeField]
     private float coinBoostAmount = 0.75f;
+
+    [Header("Sounds")]
+    [SerializeField]
+    private List<AudioClip> collectSounds;
+    [SerializeField]
+    private AudioClip boostSound;
+    private AudioSource audioPlayer;
+
+    private void Awake()
+    {
+        audioPlayer = gameObject.AddComponent<AudioSource>();
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,18 +33,27 @@ public class CoinCollector : MonoBehaviour
         {
             coins += 1;
             obj.SetActive(false);
-            Boost();
+
+            if (Boost()) audioPlayer.clip = boostSound;
+            else audioPlayer.clip = RandomCollectSound();
+            audioPlayer.Play();
         }
     }
 
-    private void Boost()
+    private bool Boost()
     {
         coinCounter += 1;
         if (coinCounter >= coinBoostCount)
         {
             GetComponent<MovementScript>().speed += coinBoostAmount;
             coinCounter = 0;
-            print("boost");
+            return true;
         }
+        else return false;
+    }
+
+    private AudioClip RandomCollectSound()
+    {
+        return collectSounds[Random.Range(0, collectSounds.Count-1)];
     }
 }
